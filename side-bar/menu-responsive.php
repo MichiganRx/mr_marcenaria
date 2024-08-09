31163605a5ed1bf210319e69ab682e345930ea10
MichiganRx <amandarodri225@gmail.com>
2024-08-09 14:22:14 -0300

    fix: adjusting animation menu responsive

--- side-bar/menu-responsive.php
+++ side-bar/menu-responsive.php
@@ -12,25 +12,25 @@
         <div class="menu" id="menu">
             <img src="./assets/img/logo.png" alt="" class="logo">
             <ul>
-                <li class="item-menu" id="home">
+                <li class="item-menu" id="home-mobile">
                     <a href="./home.php">
                         <span class="icon"><i class="bi bi-house-door"></i></span>
                         <span class="txt-link">Home</span>
                     </a>
                 </li>
-                <li class="item-menu" id="listaDeCompra">
+                <li class="item-menu" id="lista-de-compra-mobile">
                     <a href="./lista_de_compra.php" >
                         <span class="icon"><i class="bi bi-cart-check"></i></span>
                         <span class="txt-link">Lista de Compra</span>
                     </a>
                 </li>
-                <li class="item-menu" id="addUsuario">
+                <li class="item-menu" id="add-usuario-mobile">
                     <a href="./cadastro_usuario.php">
                         <span class="icon"><i class="bi bi-person-plus"></i></span>
                         <span class="txt-link">+ Usuario</span>
                     </a>
                 </li>
-                <li class="item-menu" id="addFornecedor">
+                <li class="item-menu" id="add-fornecedor-mobile">
                     <a href="./cadastro_fornecedor.php">
                         <span class="icon"><i class="bi bi-bag-check"></i></span>
                         <span class="txt-link">+ Fornecedor</span>
@@ -45,6 +45,43 @@
             </ul>
         </div>
     </div>
+    <script>
+        document.addEventListener('DOMContentLoaded', (event) => {
+            const path = window.location.pathname;
+            const page = path.split("/").pop();
+
+            const menuItems = {
+                'home.php': 'home-mobile',
+                'lista_de_compra.php': 'lista-de-compra-mobile',
+                'cadastro_usuario.php': 'add-usuario-mobile',
+                'cadastro_fornecedor.php': 'add-fornecedor-mobile',
+                'sair.php': 'sair',
+            };
+
+            if (menuItems[page]) {
+                document.getElementById(menuItems[page]).classList.add('ativo');
+            }
+        });
+        var menuItem = document.querySelectorAll('.item-menu')
+
+        function selectLink(){
+            menuItem.forEach((item)=>
+                item.classList.remove('ativo')
+            )
+            this.classList.add('ativo')
+        }
+
+        menuItem.forEach((item)=>
+            item.addEventListener('click', selectLink)
+        )
+
+        var btnExp = document.querySelector('#btn-exp')
+        var menuSide = document.querySelector('.menu-lateral')
+
+        btnExp.addEventListener('click', function(){
+            menuSide.classList.toggle('expandir')
+        })
+    </script>
     <script>
         document.getElementById('toggleBtn').addEventListener('click', function(event) {
             var menu_container = document.getElementById('menu_container');
